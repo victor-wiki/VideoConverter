15f453d0d9319001f86665dec379e2553cdba852
victor <jingwu1986@hotmail.com>
2020-09-19 12:51:20 +0800

    change default value of TaskParallelLimit from 5 to 2

--- VideoConverterCore/Model/ConvertSetting.cs
+++ VideoConverterCore/Model/ConvertSetting.cs
@@ -2,7 +2,7 @@
 {
     public class ConvertSetting
     {
-        public int TaskParallelLimit { get; set; } = 5;
+        public int TaskParallelLimit { get; set; } = 2;
         public bool EnableLog { get; set; }
         public bool EnableDebug { get; set; }
     }    
