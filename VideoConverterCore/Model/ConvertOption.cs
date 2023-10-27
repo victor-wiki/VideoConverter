8dc08be9ae068de75aa2431f4f3f861e969d422d
victor <victor-wiki@hotmail.com>
2023-10-27 12:36:57 +0800

    change

--- VideoConverterCore/Model/ConvertOption.cs
+++ VideoConverterCore/Model/ConvertOption.cs
@@ -1,13 +1,21 @@
-﻿namespace VideoConvertCore
+namespace VideoConvertCore
 {
     public class ConvertOption
     {
         public string SaveFolder { get; set; }
         public string FileType { get; set; }
+        public CustomCommandType CustomCommandType { get; set; }
         public string CustomCommand { get; set; }
         public int Quality { get; set; }
         public int? ResolutionWidth { get; set; }
-        public int? ResolutionHeight { get; set; }     
+        public int? ResolutionHeight { get; set; }
         public string Encoder { get; set; }
-    }    
+        public CutOption CutOption { get; set; }
+    }
+
+    public enum CustomCommandType
+    {
+        Custom,
+        Cut
+    }
 }
---
+++ VideoConverterCore/Model/CutOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoConvertCore
+{
+    public class CutOption
+    {
+        public double Starting { get; set; }
+        public CutType CutType { get; set; }
+        public double Duration { get; set; }
+    }
+
+    public enum CutType
+    {
+        Normal=0,
+        IntroOutro=1
+    }
+}
